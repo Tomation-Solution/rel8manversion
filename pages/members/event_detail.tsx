742e405a0621104bdaa18fca7937d55edac385ff
markothedev <markothedevmail@gmail.com>
2022-12-16 13:33:06 +0100

    added event detail to the frontend

--- pages/members/event_detail.tsx
+++ pages/members/event_detail.tsx
@@ -4,19 +4,22 @@ import { DashboardLayout } from "../../components/Dashboard/Member/Sidebar/dashb
 import Spinner from "../../components/Spinner";
 import { useMediaQuery } from 'react-responsive'
 import Table from '../../components/Table/Table'
-
+import { useAppDispatch, useAppSelector } from "../../redux/hooks";
+import { selectMemberEvent } from "../../redux/memeberEvents/memeberEventsSlice";
+import {getEventAttendies} from '../../redux/memeberEvents/memeberEventsApi'
 
 
 const EventDetail:NextPage = ()=>{
     const isLaptop = useMediaQuery({
         query: '(min-width: 524px)'
       })
+      const {attendies,status} = useAppSelector(selectMemberEvent)
+      const dispatch = useAppDispatch();
       
     useEffect(()=>{
-        //
-        console.log({localStorage})
-        if(typeof window == 'undefined'){
-            console.log(localStorage.getItem('event_detail'))
+        if(typeof window != 'undefined'){
+            const  event = JSON.parse(localStorage.getItem('event_detail'))
+            dispatch(getEventAttendies({'event_id':event.id}))
         }
       },[])
     if(typeof window == 'undefined'){
@@ -30,13 +33,30 @@ const EventDetail:NextPage = ()=>{
 
 
       const prop_columns = [ {
-        Header:'Tracking Number',
-        accessor:'paystack',
-        id:13}
+        Header:'Email',
+        accessor:'email',
+        id:1,},
+        {
+            Header:'Full Name',
+            accessor:'member_info',
+            Cell:(tableProps:any)=>(
+                <p>
+                {
+                    tableProps.row.original.member_info.find(d=>{
+                        return d.name.toLocaleLowerCase() == 'name' ||  d.name.toLocaleLowerCase() == 'first' ||d.name.toLocaleLowerCase() == 'first name' || d.name.toLocaleLowerCase() == 'surname'
+                    })['value']}</p>)
+        }
       ]
     return (
         <DashboardLayout>
-             <img src={data?.image} alt=""  style={{'width':'100%',
+
+            <div
+                style={{
+                    'maxWidth':'800px','margin':'0 auto'
+                }}
+            >
+
+<img src={data?.image} alt=""  style={{'width':'100%',
             'height':isLaptop?'400px':'300px','objectFit':'cover'}}/>
 
             <br />
@@ -45,8 +65,12 @@ const EventDetail:NextPage = ()=>{
             <p>
                 Event Status: <strong>{data.is_paid_event?'Paid':'Free'}</strong>
             </p>
+            <p>
+                Event {data.is_paid_event?'Addresse':'link'}: {data.event_access.link}
+            </p>
             <br />
-            <Table prop_columns={prop_columns} custom_data={[]}/>
+            <Table prop_columns={prop_columns} custom_data={attendies}/>
+            </div>
 
 
 
