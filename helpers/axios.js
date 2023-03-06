08f9d047b404ec46ea70b48e173c74177d507cf9
markothedev <markothedevmail@gmail.com>
2023-03-06 08:59:35 +0100

    changed mainurl to another backend

--- helpers/axios.js
+++ helpers/axios.js
@@ -3,9 +3,9 @@ import * as axiosOriginal from 'axios';
 
 //this app is only for anni so just hard code the short name here
 export const tenantName = 'man'
-export const sitename = 'web-production-d5b0.up.railway.app'
+export const sitename = 'rel8backend-production.up.railway.app'
 // export const baseURL= `https://rel8-backend-production.up.railway.app/tenant/${tenantName}`
-export const baseURL= `https://web-production-d5b0.up.railway.app/tenant/${tenantName}`
+export const baseURL= `https://rel8backend-production.up.railway.app/tenant/${tenantName}`
 const axios = axiosOriginal.create({
     baseURL,
 })
