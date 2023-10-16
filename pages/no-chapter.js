c8c97a9dfc50943254bb55af5c81aebdc38e0bc5
Nwokolo Matthew <markothedevmail@gmail.com>
2023-10-16 05:39:24 +0100

    updated the no-chapter page

--- pages/no-chapter.js
+++ pages/no-chapter.js
@@ -1,29 +1,28 @@
-import { Typography, Grid } from "@mui/material";
-import { useRouter } from "next/router";
-import GreenButton from "../components/Buttonn";
-import Navbar from '../components/Navbar'
-import FolderImage from '../images/image_processing20211130-32126-6l1sq4.jpg'
 
 export default function NoChapter(){
-    const router = useRouter()
+    // const router = useRouter()
     return(
-        <Grid container md={12} sx={{height:'90vh'}} alignContent='center' justifyContent='center'>
-            <Navbar/>
-            {/* <Grid sx={{height:}} */}
-                <Grid item  md={4} sm={10} >
-                    <img src={FolderImage.src} style={{margin:"0 auto"}} />
-                </Grid>
-                <Grid item md={12} mb={4}>
-                    <Typography textAlign='center' style={{color:'grey'}} className='text' variant='h4'>Oops!</Typography>
-                    <Typography textAlign='center' fontSize={18} style={{color:'grey'}} className='text' >Your State is not Available yet!</Typography>
-                </Grid>
-                <br/>
-                <GreenButton text='Go to Home' radius='10px'
-                textColor='white' paddingY={1} paddingX={3} bg='#2e3715'
-                click={()=>router.push('/')}
-              />
-            {/* </Grid> */}
-        </Grid>
+        // <Grid container md={12} sx={{height:'90vh'}} alignContent='center' justifyContent='center'>
+        //     <Navbar/>
+        //     {/* <Grid sx={{height:}} */}
+        //         <Grid item  md={4} sm={10} >
+        //             <img src={FolderImage.src} style={{margin:"0 auto"}} />
+        //         </Grid>
+        //         <Grid item md={12} mb={4}>
+        //             <Typography textAlign='center' style={{color:'grey'}} className='text' variant='h4'>Oops!</Typography>
+        //             <Typography textAlign='center' fontSize={18} style={{color:'grey'}} className='text' >Your State is not Available yet!</Typography>
+        //         </Grid>
+        //         <br/>
+        //         <GreenButton text='Go to Home' radius='10px'
+        //         textColor='white' paddingY={1} paddingX={3} bg='#2e3715'
+        //         click={()=>router.push('/')}
+        //       />
+        //     {/* </Grid> */}
+        // </Grid>
+
+    <div>
+
+    </div>
         
     )
 }
