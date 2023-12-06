71e1ef0469a10430b57e1281abebfab44d3674ea
Nwokolo Matthew <markothedevmail@gmail.com>
2023-12-06 22:40:52 +0100

    changed the to ws

--- helpers/axios.js
+++ helpers/axios.js
@@ -4,7 +4,7 @@ import * as axiosOriginal from 'axios';
 
 //this app is only for anni so just hard code the short name here
 export const tenantName = 'man'
-// export const sitename = 'http://localhost:8000'
+// export const sitename = 'localhost:8000'
 // export const baseURL= `http://localhost:8000/tenant/${tenantName}`
 export const sitename = 'rel8corporate.watchdoglogisticsng.com'
 export const baseURL= `https://rel8corporate.watchdoglogisticsng.com/tenant/${tenantName}`
